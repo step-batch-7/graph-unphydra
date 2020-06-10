9ba79933e85b0f3366738cb7fdd1a01ce5d62894
rivu <58026024+unphydra@users.noreply.github.com>
2020-06-10 19:07:51 +0530

    modified bfs to give true if there is a path and false if there is no path

--- src/graph.js
+++ src/graph.js
@@ -5,7 +5,21 @@
 // Should return true.
 
 const bfs = function (pairs, source, target) {
-  return true;
+  const visitedNodes = [source];
+  const queue = [source];
+  while (queue.length > 0) {
+    const node = queue.shift();
+    const childNodes = pairs.reduce((result, [from, to]) => {
+      if (from === node && !visitedNodes.includes(to)) {
+        visitedNodes.push(to);
+        result.push(to);
+      }
+      return result;
+    }, []);
+    if (childNodes.includes(target)) return true;
+    queue.push(...childNodes);
+  }
+  return false;
 };
 
 module.exports = { bfs };
